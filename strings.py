638a41e08c5ded31d7bc3c04da025c1ec34ced51
TusharSathavara <sathavaratushar456@gmail.com>
2026-07-13 00:06:06 -0600

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,20 @@
+# str = "tushar sathavara"
+# # str2 = "sathavara"
+# # final_str = str + str2
+
+# # print(str[:15]) 
+# # print(str[0:16]) 
+
+# print(str[-8:-1])
+# print(len(str))
+
+
+#     strings functions
+# str = "i am a python developer"
+
+# print(str.endswith("er"))
+
+name = input("enter your name :")
+
+print (len(name))
+print(name.count("s"))
